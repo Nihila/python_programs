572f95b7e30944d2707f0fc7a493e5cd64600d93
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 16:01:25 +0530

    Add files via upload

---
+++ player/evenandodd.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module9
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input("enter value"))
+if a%2==0:
+    print("even number")
+else:
+    print("odd number")
