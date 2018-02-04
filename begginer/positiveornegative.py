c527dfbc42f6fe9d26a3b45c045249295a2856d1
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 15:18:01 +0530

    Add files via upload

---
+++ begginer/positiveornegative.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+stringVar.count('x') - counts the number of occurrences of 'x' in stringVar
+stringVar.find('x') - returns the position of character 'x'
+stringVar.lower() - returns the stringVar in lowercase (this is temporary)
+stringVar.upper() - returns the stringVar in uppercase(this is temporary)
+stringVar.replace('a', 'b') - replaces all occurrences of a with b in the string
+stringVar.strip() - removes leading/trailing white space from string
