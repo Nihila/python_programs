314ea808567123a0b8594537e97f0a6f6ebc15bb
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 16:21:53 +0530

    Add files via upload

---
+++ player/hello5.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module18
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a="hello"
+print(a * int(5))
