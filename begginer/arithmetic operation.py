a55698f5d6a626b3b349eef3067747f111a5e942
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 15:19:33 +0530

    Add files via upload

---
+++ begginer/arithmetic operation.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+x = 1
+y = 2
+x,y = y,y
+print(x+y)
