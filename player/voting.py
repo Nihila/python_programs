1208b98e98fd52d1228f884a8a2c1d664f85c1f1
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-04 16:00:44 +0530

    Add files via upload

---
+++ player/voting.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module11
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+age=int(input("enter ur age"))
+if age>18:
+    print("you r eligible for voting")
+else:
+    print("you r not eligible for voting")
