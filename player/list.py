3029cf12db22170aec3709a066e94d984cf11355
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-09 17:38:27 +0530

    Add files via upload

---
+++ player/list.py
@@ -0,0 +1,19 @@
+n=[1,2,3,4,5,6,7,8,9]
+odd_number=0
+even_number=0
+for x in n:
+    if(x%2==0):
+        even_number=even_number+1
+    else:
+        odd_number=odd_number+1
+print("no.of even numbers :")
+print(even_number)
+print("no.of odd numbers :")
+print(odd_number)
+        
+
+
+
+
+
+
