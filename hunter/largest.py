a3ba1520aa0e169f3ee46c518c055a35c2c2991c
Nihila <36109188+Nihila@users.noreply.github.com>
2018-02-17 07:58:32 +0530

    Add files via upload

---
+++ hunter/largest.py
@@ -0,0 +1,9 @@
+a=input("enter the 1st numbers :")
+b=input("enter the 2nd number :")
+c=input("enter the 3rd number :")
+if((a>b) and (a>c)):
+    print("The largest number is :",a )
+elif((b>a) and (b>c)):
+    print("The largest number is :",b)
+else:
+    print("The largest number is :",c)
