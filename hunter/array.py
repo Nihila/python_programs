b32dca4222e8a173afc0c625fd7c4d7511352d82
Nihila <36109188+Nihila@users.noreply.github.com>
2018-03-03 17:52:58 +0530

    Add files via upload

---
+++ hunter/array.py
@@ -0,0 +1,11 @@
+n=int(input("enter the value :"))
+r=int(input("enter the value :"))
+a=[]
+sum=0
+for i in range(n):
+     l=int(input("enter the value :"))
+     a.append(l)
+for i in range(r):
+     sum=sum+a[i]
+print(sum)    
+     
