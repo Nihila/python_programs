72f0858ac59c118cf0c65516562b5ca897069c42
Nihila <36109188+Nihila@users.noreply.github.com>
2018-03-16 17:29:21 +0530

    Add files via upload

---
+++ hunter/prime.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value :"))
+for i in range (2,a):
+        if(a%i==0):
+            print(" not a prime number")
+            break
+        else:
+            print("prime number")
+            break
+ 
